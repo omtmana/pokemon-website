104acf9d978fe3d3f4ab2b00056e99a6c6e1bfe6
Olivia <oliviatmanalastas@gmail.com>
2022-06-28 11:22:47 -0400

    Add name

--- main.js
+++ main.js
@@ -2,31 +2,38 @@
 // we use Js to manipulate HTML
 //create html consten with 
 
-const pokemon = ['charmander', 'bulbasaur', 'squirtle'];
+//const pokemon = ['bulbasaur', 'charmander', 'squirtle'];
 const pokemonIDs = ['001', '004', '007'];
+
+const pokemon = [
+    {name: 'bulbasaur', id: '001'},
+    {name: 'charmander', id: '004'},
+    {name: 'squirtle', id: '007'},
+]
+
 const containerDiv = document.querySelector('#container');
 
-/* loop over every single ID 
-create an HTML element - 
+ pokemon.map((element, index) => {
+     console.log(element);
+     let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${element.id}.png`
+    //  console.log('img Url', imgUrl)
+   
+     let div = document.createElement('div') // adding a div to house the img element
+     let h3 = document.createElement('h3')
+     h3.innerText = element.name //adds the name inder the picture
+     div.setAttribute('class', 'pokemon-card') // setting the class of the div
+     let img = document.createElement('img') // creating the img element itself
+     img.src = imgUrl
+     div.append(img, h3) //append the img element 
+     containerDiv.append(div)
+ })
+ 
+/* loop over every single ID
+create an HTML element -
  set the HTML element values
  append HTML element to the DOM
 
  */
 
  //grab img URL
- //create variable called imgUrl -- use backticks 
-//
- pokemonIDs.map((id) => {
-     let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
-     console.log('img Url', imgUrl)
-     let div = document.createElement('div') // adding a div to house the img element
-     div.setAttribute('class', 'pokemon-card') // setting the class of the div
-     let img = document.createElement('img') // creating the img element itself
-     img.src = imgUrl
-     div.append(img) //append the img element 
-     containerDiv.append(div)
- })
- 
-
- //pin them/wrap them in container
- //
+ //create variable called imgUrl -- use backticks
